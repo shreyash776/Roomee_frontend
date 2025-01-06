a390156ed8f641e86837ace09786d60f59fcba2e
shreyash776 <shreyashchaurasia77@gmail.com>
2025-01-06 17:55:43 +0530

    refactor ChatScreen layout: change background color to white and adjust user card layout

--- app/(tabs)/chat.tsx
+++ app/(tabs)/chat.tsx
@@ -1,15 +1,15 @@
 import { Link } from 'expo-router';
 import React from 'react';
-import { ScrollView, Text ,TouchableOpacity } from 'react-native';
+import { ScrollView, Text ,TouchableOpacity ,View } from 'react-native';
 import tw from 'twrnc';
 import UserCard from '../components/userCard'; 
 import { dummyData } from '../data/dummyData'; 
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
+
 
 const ChatScreen: React.FC = () => {
   return (
     <ScrollView 
-      contentContainerStyle={tw`bg-black pt-12 px-6`} 
+      contentContainerStyle={tw`bg-white pt-12 px-6`} 
       showsVerticalScrollIndicator={false}
     >
       {/* Heading */}
@@ -22,7 +22,7 @@ const ChatScreen: React.FC = () => {
         Connect with verified renters to discuss your housing needs and find the perfect living arrangement.
       </Text>
 
-     <View style={tw`flex-row flex-wrap`}>
+     <View style={tw`flex-column justify-center items-center gap-5`}>
        {/* User Cards */}
        {dummyData.map((data, index) => (
         <Link href={`/screens/user/${data.id}`}>
