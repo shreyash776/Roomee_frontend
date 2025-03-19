a7dc1cd54060edd4d44b6a54f16236fd4d247cf5
shreyash776 <shreyashchaurasia77@gmail.com>
2025-03-20 00:26:35 +0530

    feat: export all services from the index file for easier access

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+// Exporting all services 
+export * from './api';
+export * from './api-client';
+export * from './auth-storage';
+export * from './auth-service';
+export * from './profile-service';
+export * from './room-service';
